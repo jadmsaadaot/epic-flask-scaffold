51de3401e0cd264ce03b1c608ffa8010e78c3e61
Jad <jad.saad@aot-technologies.com>
2024-06-27 15:18:19 -0700

    stable scaffold without manage.py

--- scaffold-api/entrypoint.sh
+++ scaffold-api/entrypoint.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-
-python manage.py db upgrade && python wsgi.py
+flask db upgrade && python wsgi.py
--- scaffold-api/wsgi.py
+++ scaffold-api/wsgi.py
@@ -1,10 +1,6 @@
-# from flask_migrate import Migrate
-
 from scaffold_api import create_app
 
 application = create_app()
-# migrate = Migrate(application, db)
-
 
 if __name__ == "__main__":
     application.run(debug=True, host='0.0.0.0', port=5000)
